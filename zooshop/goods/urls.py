15f67668bd9e822ff3caef7207c0e703a27ef415
YaroslavSavchinskiy <savcinskijaroslav35@gmail.com>
2026-06-08 18:54:14 +0300

    Delete zooshop directory

--- zooshop/goods/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'goods'
-urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('category/<slug:slug>/', views.category_view, name='category'),
-    path('product/<slug:slug>/', views.product_detail_view, name='product_detail'),
-]
