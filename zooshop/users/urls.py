f6144e1876f35fc24eb0344e220e7f3ed9728e0b
YaroslavSavchinskiy <savcinskijaroslav35@gmail.com>
2026-06-09 12:47:01 +0300

    Delete zooshop directory

--- zooshop/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'users'
-
-urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('profile/', views.profile_view, name='profile')
-]
